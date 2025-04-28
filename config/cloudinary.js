49beeaf1cbfa490229ce1fe7c40d35708f4810c3
Aditya Sharma <adityasharma96655@gmail.com>
2025-04-28 22:09:26 +0530

    Add files via upload

---
+++ config/cloudinary.js
@@ -0,0 +1,9 @@
+import { v2 as cloudinary } from 'cloudinary';
+
+cloudinary.config({
+  cloud_name: 'desgqymup',
+  api_key: '882436726576889',
+  api_secret: 'SZuO_kmVq-vcWMe16GkDfBoxFz0'
+});
+
+export default cloudinary;
