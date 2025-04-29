64037a55af859585d73d327cc9e7299166270050
Aditya Sharma <adityasharma96655@gmail.com>
2025-04-29 12:22:31 +0530

    Update cloudinary.js

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,9 +1,13 @@
 import { v2 as cloudinary } from 'cloudinary';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 cloudinary.config({
-  cloud_name: 'desgqymup',
-  api_key: 'xxxxxxxxxxxxx9',
-  api_secret: 'xxxxxxxxxxxxxxxxxFz0'
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 export default cloudinary;
+
